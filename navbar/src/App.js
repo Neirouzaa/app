fb83d2b3a110ddca48f9052b9cf8e3b0e0da9bf7
Neirouzaa <neirouz.elhouichet@gmail.com>
2024-05-06 19:50:30 +0100

    first commit

---
+++ navbar/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Body from "./body/body";
+import Navbar from './navbar/navbar.jsx';
+
+
+function App() {
+  return (
+
+    <div className="App">
+      hello
+      <Navbar/>
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
